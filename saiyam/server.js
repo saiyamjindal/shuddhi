8543c1700188decf8bfbf3c389eab4ce86f6ae4c
Saiyam <jindal.saiyam123@gmail.com>
2020-05-30 14:12:07 +0530

    files

---
+++ saiyam/server.js
@@ -0,0 +1,173 @@
+const MONGO_DB ="mongodb+srv://creativeprotocol:creativeprotocol@cluster0-kdlho.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"; 
+// const SK = process.env.SK;
+var express = require('express')
+var app = express()
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+var fs = require('fs')
+var path = require('path')
+const mongoose = require('mongoose');
+mongoose.connect(MONGO_DB, { useNewUrlParser: true });
+var session = require('express-session')
+var _ = require("lodash")
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+const crypto = require('crypto');
+const url = require('url');
+
+var bodyParser = require("body-parser")
+// var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 1160000 } }))
+const secret = 'abcdefg';
+
+const UserSchema = new Schema({
+    name: String,
+    regno : {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    regcert:{
+        type: String,
+        default: "/default.png",
+        required: true
+    },
+    cert12a:{
+        type: String,
+        default: "/default.png",
+        required: true
+    },
+    cert80g:{
+        type: String,
+        default: "/default.png",
+        required: true
+    },
+    fcra:{
+        type: String,
+        default: "/default.png",
+        required: true
+    },
+    acname:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    acno: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+    ifsccode:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    bankadd:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    authperson:{
+        type: String,
+        unique: true,
+        required: true
+    },
+    phno:{
+        type: Number,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required:true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+      },
+      confirmPassword: {
+        type: String,
+        validate: function () {
+          return this.password == this.confirmPassword;
+        },
+        required: true,
+      },
+      description:{
+        type: String,
+        unique: true,
+        required:true
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+app.get('/register', function (req, res) {
+    res.render('register')
+})
+app.post('/register', urlencodedParser, function (req, res) {
+    let newUser = new User();
+    newUser.name = req.body.name;
+    newUser.regno = req.body.regno;
+
+
+    newUser.acname = req.body.acname;
+    newUser.acno = req.body.acno;
+    newUser.ifsccode = req.body.ifsccode;
+    newUser.bankadd = req.body.bankadd;
+    newUser.authperson = req.body.authperson;
+    newUser.phno = req.body.phno;
+    newUser.email = req.body.email;
+    newUser.password = req.body.password;
+    newUser.description = req.body.description;
+
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err, 'error')
+            return
+        }
+        res.redirect('/success')
+
+    });
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server has started at port 3000");
+  });
+
+
+
+  module.exports.updateProfileImage = async function updateProfileImage(req, res) {
+    // update anything
+    //  form data 
+    try {
+      // console.log(req.file);
+      let serverPath = `public/img/users/user-${Date.now()}.jpeg`
+      // process
+      console.log("I was here");
+      await sharp(req.file.path)
+        .resize(200, 200)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(serverPath);
+      serverPath = serverPath.split("/").slice(1).join("/");
+      
+      let user = await userModel.findById(req.id);
+      
+      user.profileImage = serverPath;
+  
+      await user.save({ validateBeforeSave: false });
+      // console.log("I was here");
+      res.status(200).json({
+        status: "image uploaded"
+      })
+    } catch (err) {
+      console.log(err);
+      console.log(err.message);
+    }
+  }
+  
